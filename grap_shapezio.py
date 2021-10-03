bb073b72702d57282135af7f15396068453f1e01
swear01 <stanley.yellow1@gmail.com>
2021-10-03 12:37:10 +0800

    failed. Too slow. IDK why

--- grap_shapezio.py
+++ grap_shapezio.py
@@ -1,7 +1,6 @@
 from re import S
-import praw
-from psaw import PushshiftAPI
-from datetime import datetime
+from pmaw import PushshiftAPI
+from datetime import datetime, tzinfo, timezone
 import json
 from tqdm import tqdm
 from pathlib import Path 
@@ -9,34 +8,46 @@ from pathlib import Path
 sub_name = "shapezio"
 submission_limit = 10
 year = 2020
+workers = 60
 
+'''
 reddit = praw.Reddit(
     client_id = "J7tuNylJ6qvYQNXMhSrM2g",
     client_secret = "f1OhB2biWcbKKyoF98AWeQGkbtnovA",
-    user_agent = "windows:grap_bot (by u/swear01)"
+    user_agent = "windows:grap_bot (by u/swear01)",
 )
-api = PushshiftAPI(reddit)
+#'''
+
+api = PushshiftAPI(num_workers = workers) #we don't need enrichment
 p = Path(".")
 if not (p / sub_name).is_dir() :(p/sub_name).mkdir()
-
-for month in tqdm(range(4,12)) :
-    time_start = int(datetime(year,month,1).timestamp()) 
-    time_end = int(datetime(year,month+1,1).timestamp())
+time_start = int(datetime(year,1,1,tzinfo=timezone.utc).timestamp()) 
+time_end = int(datetime(year+1,1,1,tzinfo=timezone.utc).timestamp()) 
+print(time_start,time_end)
+epoch = 0  
+while 1 :
     stored = []
-    for submission_id in tqdm(api.search_submissions(
+    submission_ids = list(api.search_submissions(
+        subreddit = sub_name,        
         after = time_start,
         before = time_end,
-        subreddit = sub_name,
-        #num_comments = 1,
-        #filter = [] ,
-    )):        
+        limit = 10,
+        mem_safe = True
+    ))
+    print(1,epoch)
+    for submission_id in tqdm(submission_ids):  
+        print(4,epoch)      
         s_data = []
-        submission = reddit.submission(submission_id) 
-        s_data.append(str(submission.id))
-        submission.comments.replace_more(limit=None) ; #get a list of comment tree
-        for comment_t in submission.comments :
-            s_data.append([comment_t.id,comment_t.body,comment_t.score])
-        stored.append(s_data) ;
+        s_data.append(str(submission_id))
+        comment_ids = api.search_submission_comment_ids(ids=submission_id)
+        print(5,epoch)  
+        for comment in tqdm(api.search_comments(ids=comment_ids)) :
+            print(3,epoch)
+            s_data.append([comment["id"],comment["body"],comment["score"]])
+        stored.append(s_data) 
+    print(2,epoch)
+    with open(f"./{sub_name}/{epoch}.json","w") as f :
+        json.dump(stored,f,indent=2) 
 
-    with open(f"./{sub_name}/{year}_{month}.json","w") as f :
-        json.dump(stored,f,indent=2) ;
+    if api.search_submissions(ids=submission_ids[-1]).time >= time_end : break
+    epoch+=1
