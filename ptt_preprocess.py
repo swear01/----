73bcfb0b2c7d95b9d28d76f010e531eb8c69dc0a
Swear01 <stanley.yellow1@gmail.com>
2021-11-11 18:59:00 +0800

    Finally Finish The PreProcess

---
+++ ptt_preprocess.py
@@ -0,0 +1,41 @@
+import json
+import pathlib
+import regex
+from pathlib import Path
+
+source = "./ptt/politics/"
+epoch=0
+dest = "./"
+
+pdata_p = []
+with open(f"{source}{epoch}.json","r",encoding="utf-8") as f :
+    pdata= json.load(f) 
+
+for sub in pdata:
+    if not sub : continue
+    # print(sub)
+    cdata_p=[]
+    temp = {}
+    for comment in sub['comments'] :
+        if not comment : continue
+        if not comment['comment'] : continue
+        if temp and temp['user'] == comment['user'] : 
+            comment['comment'] = temp['comment'] + comment['comment']
+        else:
+            #if 'comment' not in temp : continue
+            #temp['comment'] = regex.sub('\n','',temp['comment'])
+            if temp :
+                cdata_p.append(temp)
+        temp = comment
+    sub['comments'] = cdata_p ;
+    sub['content'] = regex.sub('\n','',sub['content'])
+    sub['content'] = regex.sub('\s','',sub['content'])
+    pdata_p.append(sub) ;
+
+with open(f"{dest}{epoch}.json","w",encoding="utf-8") as f :
+    json.dump(pdata,f,indent=2,ensure_ascii=False) 
+
+    
+             
+
+        
